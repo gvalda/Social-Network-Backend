80a53924388c998bb243f0efcb952016d8a3bfd4
yogoh31 <58604899+yogoh31@users.noreply.github.com>
2021-11-09 18:54:46 +0200

    Collected static

--- instagram/settings.py
+++ instagram/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 from datetime import timedelta
 from pathlib import Path
 
@@ -176,3 +177,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
