13b79ed827348d3781b40a30ce20c9e210e67296
yogoh31 <58604899+yogoh31@users.noreply.github.com>
2021-11-09 18:56:50 +0200

    Removed Heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -1,4 +1,3 @@
-import django_heroku
 from datetime import timedelta
 from pathlib import Path
 
@@ -177,5 +176,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
