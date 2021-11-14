e49361cc0104861298b072677fe20f040cbda848
yogoh31 <58604899+yogoh31@users.noreply.github.com>
2021-11-14 19:40:19 +0200

    Added cors library

--- instagram/settings.py
+++ instagram/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'tags.apps.TagsConfig',
     'comments.apps.CommentsConfig',
     'storages',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -73,7 +74,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'instagram.urls'
 
